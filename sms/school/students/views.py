5257ee0fb54eecdd9cf3e41bdda8472939aedc9c
jabir254 <jabirkullow@gmail.com>
2023-07-01 20:07:23 +0300

    added view student function

--- sms/school/students/views.py
+++ sms/school/students/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.urls import reverse
 from django.shortcuts import render
 from . models import Student
 # Create your views here.
@@ -7,3 +9,6 @@ def index(request):
     return render(request, 'students/index.html',{
         'students': Student.objects.all()
         })
+
+def view_student(request, id):
+    student = Student.objects.get(pk=10)
