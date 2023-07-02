39b13f716c96f0b69456b9e2ebed3667bb0f587a
jabir254 <jabirkullow@gmail.com>
2023-07-02 09:51:13 +0300

    cleared minor bugs on views and urls files

--- sms/school/students/views.py
+++ sms/school/students/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import render
 from . models import Student
